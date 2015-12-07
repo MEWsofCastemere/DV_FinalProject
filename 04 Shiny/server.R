4d7a5c8b8cf14bbbe18b3318b56dbd63458d9f72
Adam Barrett <ke5gpd@gmail.com>
2015-12-07 00:21:04 -0600

    no message

---
+++ 04 Shiny/server.R
@@ -0,0 +1,116 @@
+# server.R
+require("jsonlite")
+require("RCurl")
+require(ggplot2)
+require(dplyr)
+require(shiny)
+require(shinydashboard)
+require(leaflet)
+require(gridExtra)
+
+shinyServer(function(input, output) {
+  
+  KPI_Low_Max_value <- reactive({input$KPI1}) 
+  
+  df1 <- eventReactive(input$clicks, {data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from ADAMDATA"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_mew2795', PASS='orcl_mew2795', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))) %>% group_by() })
+  
+  output$distPlot1 <- renderPlot({             
+    plot <- ggplot() + 
+      coord_cartesian() + 
+      scale_x_discrete() +
+      scale_y_discrete() +
+      scale_fill_gradient2(low="white", mid = "red", high= "darkred", midpoint = 7500) +
+      labs(title=input$title) +
+      labs(x=paste("Year"), y=paste("State")) +
+      layer(data=df1(), 
+            mapping=aes(x=YEAR, y=STATE, fill = DAMAGE_KPI), 
+            stat="identity", 
+            stat_params=list(), 
+            geom="tile",
+            geom_params=list(), 
+            position=position_identity()
+      )
+    
+    plot
+  })
+  observeEvent(input$clicks, {
+    print(as.numeric(input$clicks))
+  })
+  
+  # Begin code for Second Tab:
+  dfs <- eventReactive(input$clicks2, {data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from STORMEVENTS"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_mew2795', PASS='orcl_mew2795', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), )) %>% select(BEGIN_YEARMONTH, DEATHS_DIRECT, DEATHS_INDIRECT, DAMAGE_PROPERTY, BEGIN_DAY) %>% filter(DEATHS_DIRECT != 0)})
+  #dfs1 <- select(dfs, BEGIN_YEARMONTH, DEATHS_DIRECT, DEATHS_INDIRECT, DAMAGE_PROPERTY, BEGIN_DAY) %>% filter(DEATHS_DIRECT != 0)
+  #df2 <- eventReactive(input$clicks2, {dfs1})
+  
+  output$distPlot2 <- renderPlot(height=1000, width=2000, {
+    plot1 <- ggplot() + 
+      coord_cartesian() + 
+      scale_x_continuous() +
+      scale_y_continuous() +
+      facet_wrap(~BEGIN_DAY, ncol = 1) +
+      labs(title='StormEvents Barchart\ndeaths_direct, avg(deaths_direct), ') +
+      labs(x=paste("Begin Day"), y=paste("Deaths Direct")) +
+      layer(data=dfs(), 
+            mapping=aes(x=BEGIN_DAY, y=(DEATHS_DIRECT)), 
+            stat="identity", 
+            stat_params=list(), 
+            geom="bar",
+            geom_params=list(colour="blue"), 
+            position=position_identity()
+      ) + coord_flip() +
+      layer(data=dfs(), 
+            mapping=aes(x=BEGIN_DAY, y=DEATHS_DIRECT, label=(DEATHS_DIRECT)), 
+            stat="identity", 
+            stat_params=list(), 
+            geom="text",
+            geom_params=list(colour="black", hjust=0.5), 
+            position=position_identity()
+      ) +
+      layer(data=dfs(), 
+            mapping=aes(yintercept = mean(DEATHS_DIRECT)), 
+            geom="hline",
+            geom_params=list(colour="red")
+      )
+    plot1
+  })
+  
+  # Begin code for Third Tab:
+  
+  df4 <- eventReactive(input$clicks4, {data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select EVENT_TYPE, DAMAGE_CROPS, DAMAGE_PROPERTY, END_TIME, BEGIN_TIME from STORMEVENTS where DAMAGE_CROPS is not null and DAMAGE_PROPERTY is not null"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_jdg3666', PASS='orcl_jdg3666', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))  %>% select(EVENT_TYPE, DAMAGE_CROPS, DAMAGE_PROPERTY, END_TIME, BEGIN_TIME) %>% filter(DAMAGE_PROPERTY > 0, DAMAGE_CROPS > 0) %>% mutate (TOTAL_TIME = (abs(END_TIME - BEGIN_TIME)) / 100) %>% filter(TOTAL_TIME > 0) })
+  
+  output$distPlot4 <- renderPlot({ 
+    crop <- ggplot() + 
+      coord_cartesian() + 
+      scale_x_discrete() +
+      scale_y_continuous() +
+      labs(title='Crop and Property Damage across Disasters less than 24 hours') +
+      labs(x="Total Time of Disaster", y=paste("Crop Damage")) +
+      layer(data=df4(), 
+            mapping=aes(as.numeric(TOTAL_TIME), y=as.numeric(as.character(DAMAGE_CROPS)), color=EVENT_TYPE), 
+            stat="identity", 
+            stat_params=list(), 
+            geom="point",
+            geom_params=list(), 
+            #position=position_identity()
+            position=position_jitter(width=0.3, height=0)
+      )
+    
+    property <- ggplot() + 
+      coord_cartesian() + 
+      scale_x_discrete() +
+      scale_y_continuous() +
+      labs(title='Crop and Property Damage across Disasters less than 24 hours') +
+      labs(x="Total Time of Disaster", y=paste("Crop Damage")) +
+      layer(data=df4(), 
+            mapping=aes(as.numeric(TOTAL_TIME), y=as.numeric(as.character(DAMAGE_PROPERTY)), color=EVENT_TYPE), 
+            stat="identity", 
+            stat_params=list(), 
+            geom="point",
+            geom_params=list(), 
+            #position=position_identity()
+            position=position_jitter(width=0.3, height=0)
+      )
+    plot4 <- grid.arrange(crop, property)
+    plot4
+  })
+})
---
+++ 04 Shiny/ui.R
@@ -0,0 +1,38 @@
+#ui.R 
+
+library(shiny)
+
+navbarPage(
+  title = "Storm Event Shiny App",
+  tabPanel(title = "Crosstab",
+           sidebarPanel(
+             sliderInput("KPI1", "KPI Cutoff:", 
+                         min = 1, max = 2600,  value = 80),
+             textInput(inputId = "title", 
+                       label = "Crosstab Title",
+                       value = "Damage KPI by State and Type"),
+             actionButton(inputId = "clicks", 
+                          label = "Generate Plot")
+           ),
+           
+           mainPanel(plotOutput("distPlot1")
+           )
+  ),
+  tabPanel(title = "Barchart",
+           sidebarPanel(
+             actionButton(inputId = "clicks2",  label = "Click me")
+           ),
+           
+           mainPanel(plotOutput("distPlot2")
+           )
+  ),
+  
+  tabPanel(title = "Scatter Plot",
+           sidebarPanel(
+             actionButton(inputId = "clicks4", label = "Click me")
+           ),
+           mainPanel(plotOutput("distPlot4")
+           )
+  )
+)
+
