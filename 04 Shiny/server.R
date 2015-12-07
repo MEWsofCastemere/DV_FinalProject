241a7150f66e67678348977e6b04feb5a8f35c60
MEWsofCastemere <malachiw@utexas.edu>
2015-12-07 12:11:55 -0600

    no message

--- 04 Shiny/server.R
+++ 04 Shiny/server.R
@@ -38,20 +38,86 @@ shinyServer(function(input, output) {
   })
   
   # Begin code for Second Tab:
-  dfs <- eventReactive(input$clicks2, {data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from STORMEVENTS"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_mew2795', PASS='orcl_mew2795', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), )) %>% select(BEGIN_YEARMONTH, DEATHS_DIRECT, DEATHS_INDIRECT, DAMAGE_PROPERTY, BEGIN_DAY) %>% filter(DEATHS_DIRECT != 0)})
+  dfs <- eventReactive(input$clicks2, {data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from ADAMDATA"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_mew2795', PASS='orcl_mew2795', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), )) %>% select(YEAR, DEATHS_DIRECT, DEATHS_INDIRECT, DAMAGE_PROPERTY, DAMAGE_CROPS, INJURIES_DIRECT, INJURIES_INDIRECT, STATE) %>% mutate(TOTAL_INJURIES = INJURIES_DIRECT + INJURIES_INDIRECT)})
   #dfs1 <- select(dfs, BEGIN_YEARMONTH, DEATHS_DIRECT, DEATHS_INDIRECT, DAMAGE_PROPERTY, BEGIN_DAY) %>% filter(DEATHS_DIRECT != 0)
   #df2 <- eventReactive(input$clicks2, {dfs1})
   
-  output$distPlot2 <- renderPlot(height=1000, width=2000, {
-    plot1 <- ggplot() + 
+  output$distPlot2 <- renderPlot(height=1000, width=1000, {
+    DDPY <- ggplot() + 
       coord_cartesian() + 
-      scale_x_continuous() +
+      scale_x_discrete() +
       scale_y_continuous() +
-      facet_wrap(~BEGIN_DAY, ncol = 1) +
+      facet_wrap(~YEAR, ncol = 1) +
       labs(title='StormEvents Barchart\ndeaths_direct, avg(deaths_direct), ') +
-      labs(x=paste("Begin Day"), y=paste("Deaths Direct")) +
+      labs(x=paste("Year"), y=paste("Deaths Direct")) +
       layer(data=dfs(), 
-            mapping=aes(x=BEGIN_DAY, y=(DEATHS_DIRECT)), 
+            mapping=aes(x=YEAR, y=(DEATHS_DIRECT)), 
+            stat="identity", 
+            stat_params=list(), 
+            geom="bar",
+            geom_params=list(color="blue"), 
+            position=position_identity()
+      ) + coord_flip() +
+      layer(data=dfs(), 
+            mapping=aes(yintercept = 33.9), 
+            geom="hline",
+            geom_params=list(colour="red")
+      )
+    DDPY
+    IDPY <- ggplot() + 
+      coord_cartesian() + 
+      #scale_color_gradient2(mid = ("Green"), high="Red") +
+      scale_x_discrete() +
+      scale_y_continuous() +
+      facet_wrap(~YEAR, ncol = 1) +
+      labs(title='StormEvents Barchart\ndeaths_indirect, avg(deaths_indirect), ') +
+      labs(x=paste("Year"), y=paste("Deaths Indirect")) +
+      layer(data=dfs(), 
+            mapping=aes(x=YEAR, y=(DEATHS_INDIRECT)), 
+            stat="identity", 
+            stat_params=list(), 
+            geom="bar",
+            geom_params=list(color="blue"), 
+            position=position_identity()
+      ) + coord_flip() +
+      layer(data=dfs(), 
+            mapping=aes(yintercept = 1.33), 
+            geom="hline",
+            geom_params=list(colour="red")
+      )
+    IDPY
+    DCPY <- ggplot() + 
+      coord_cartesian() + 
+      #scale_color_gradient2(mid = ("Green"), high="Red") +
+      scale_x_discrete() +
+      scale_y_continuous() +
+      facet_wrap(~YEAR, ncol = 1) +
+      labs(title='StormEvents Barchart\nDAMAGE_CROPS, avg(DAMAGE_CROPS), ') +
+      labs(x=paste("Year"), y=paste("Damage Crops")) +
+      layer(data=dfs(), 
+            mapping=aes(x=YEAR, y=(DAMAGE_CROPS)), 
+            stat="identity", 
+            stat_params=list(), 
+            geom=("bar"),
+            geom_params=list(colour="blue"), 
+            position=position_identity()
+      ) + coord_flip() +
+      layer(data=dfs(), 
+            mapping=aes(yintercept = 266), 
+            geom="hline",
+            geom_params=list(colour="red")
+      )
+    DCPY
+    DPPY <- ggplot() + 
+      coord_cartesian() + 
+      #scale_color_gradient2(mid = ("Green"), high="Red") +
+      scale_x_discrete() +
+      scale_y_continuous() +
+      facet_wrap(~YEAR, ncol = 1) +
+      labs(title='StormEvents Barchart\nDAMAGE_PROPERTY, avg(DAMAGE_PROPERTY), ') +
+      labs(x=paste("Year"), y=paste("DAMAGE PROPERTY")) +
+      layer(data=dfs(), 
+            mapping=aes(x=YEAR, y=(DAMAGE_PROPERTY)), 
             stat="identity", 
             stat_params=list(), 
             geom="bar",
@@ -59,18 +125,12 @@ shinyServer(function(input, output) {
             position=position_identity()
       ) + coord_flip() +
       layer(data=dfs(), 
-            mapping=aes(x=BEGIN_DAY, y=DEATHS_DIRECT, label=(DEATHS_DIRECT)), 
-            stat="identity", 
-            stat_params=list(), 
-            geom="text",
-            geom_params=list(colour="black", hjust=0.5), 
-            position=position_identity()
-      ) +
-      layer(data=dfs(), 
-            mapping=aes(yintercept = mean(DEATHS_DIRECT)), 
+            mapping=aes(yintercept = 2000), 
             geom="hline",
             geom_params=list(colour="red")
       )
+    DPPY
+    plot1 <- grid.arrange(DDPY, IDPY, DCPY, DPPY)
     plot1
   })
   
